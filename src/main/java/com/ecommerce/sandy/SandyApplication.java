7b71b03926d3f84cac1112bb0b14f3a0bbff716a
Sandy Andryanto <sandyandryanto@gmail.com>
2020-11-22 13:07:41 +0700

    first commit

---
+++ src/main/java/com/ecommerce/sandy/SandyApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.sandy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandyApplication.class, args);
+    }
+
+}
